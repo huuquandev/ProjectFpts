ecf527416e6e9c8ed71e3386e33061a4b8469d30
huuquandev <viphuy12@gmail.com>
2023-07-28 16:24:14 +0700

    Update them phan sap xep voi danh muc

--- WebSiteBanHang/Controllers/SanPhamController.cs
+++ WebSiteBanHang/Controllers/SanPhamController.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Diagnostics;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using WebSiteBanHang.Commons;
 using WebSiteBanHang.Models;
 
@@ -29,44 +31,125 @@ namespace WebSiteBanHang.Controllers
             var danhSachSanPham = db.Products.ToList();
             return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
-        public ActionResult mat_ong_nhap_khau()
+        public ActionResult mat_ong_nhap_khau(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
-        public ActionResult mat_ong_rung()
+        public ActionResult mat_ong_rung(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
-        public ActionResult mat_ong_rung_nhiet_doi()
+        public ActionResult mat_ong_rung_nhiet_doi(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
-        public ActionResult mat_ong_rung_hoa()
+        public ActionResult mat_ong_rung_hoa(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
-        public ActionResult mat_ong_rung_ngap_man()
+        public ActionResult mat_ong_rung_ngap_man(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
 
-        public ActionResult mat_ong_hoa()
+        public ActionResult mat_ong_hoa(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
 
-        public ActionResult mat_ong_thien_nhien()
+        public ActionResult mat_ong_thien_nhien(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
 
-        public ActionResult mat_ong_nguyen_chat()
+        public ActionResult mat_ong_nguyen_chat(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
-        public ActionResult mat_ong_hoa_sam_ngoc_linh()
+        public ActionResult mat_ong_hoa_sam_ngoc_linh(int? page, int? pageSize, int loaiSanPhamId)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            var danhSachSanPham = db.Products.Where(p => p.idloaisanpham == loaiSanPhamId).ToList();
+            return View(danhSachSanPham.ToPagedList((int)page, (int)pageSize));
         }
         public ActionResult chiTietSanPham(int productId)
         {
@@ -74,5 +157,45 @@ namespace WebSiteBanHang.Controllers
 
             return View(objProduct);
         }
+        public ActionResult SortProducts(string sortBy, int? page, int? pageSize)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+
+            var danhSachSanPham = db.Products.ToList();
+
+            switch (sortBy)
+            {
+                case "alpha-asc":
+                    danhSachSanPham = danhSachSanPham.OrderBy(p => p.ten).ToList();
+                    break;
+                case "alpha-desc":
+                    danhSachSanPham = danhSachSanPham.OrderByDescending(p => p.ten).ToList();
+                    break;
+                case "price-asc":
+                    danhSachSanPham = danhSachSanPham.OrderBy(p => p.giatien).ToList();
+                    break;
+                case "price-desc":
+                    danhSachSanPham = danhSachSanPham.OrderByDescending(p => p.giatien).ToList();
+                    break;
+                case "created-asc":
+                    danhSachSanPham = danhSachSanPham.OrderBy(p => p.ngaynhap).ToList();
+                    break;
+                case "created-desc":
+                    danhSachSanPham = danhSachSanPham.OrderByDescending(p => p.ngaynhap).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            return PartialView("PartialProductList", danhSachSanPham.ToPagedList((int)page, (int)pageSize));
+        }
+
     }
 }
